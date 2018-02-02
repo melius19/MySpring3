a0862faffcc33e8088d58f4ca7fa70d6e798f562
melius19 <melius19@protonmail.com>
2018-02-02 21:20:25 +0900

    ok

---
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,83 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.SearchCriteria;
+import org.zerock.persistence.BoardDAO;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+	@Inject
+	private BoardDAO dao;
+
+	@Transactional
+	@Override
+	public void regist(BoardVO board) throws Exception {
+		dao.create(board);
+
+		String[] files = board.getFiles();
+		System.out.println("BSRV1:");
+		if (files == null) {
+			return;
+		}
+		System.out.println("BSRV2:");
+		for (String fileName : files) {
+			System.out.println("BSRV3:");
+			System.out.println("BSRV3:");
+			dao.addAttach(fileName);
+		}
+	}
+
+	@Transactional(isolation = Isolation.READ_COMMITTED)
+	@Override
+	public BoardVO read(Integer bno) throws Exception {
+		dao.updateViewCnt(bno);
+		return dao.read(bno);
+	}
+
+	public void modify(BoardVO board) throws Exception {
+		dao.update(board);
+	}
+
+	public void remove(Integer bno) throws Exception {
+		dao.delete(bno);
+	}
+
+	public List<BoardVO> listAll() throws Exception {
+		return dao.listAll();
+	}
+
+	@Override
+	public List<BoardVO> listCriteria(Criteria cri) throws Exception {
+		return dao.listCriteria(cri);
+	}
+
+	@Override
+	public int listCountCriteria(Criteria cri) throws Exception {
+		return dao.countPaging(cri);
+	}
+
+	@Override
+	public List<BoardVO> listSearchCriteria(SearchCriteria cri) throws Exception {
+		return dao.listSearch(cri);
+	}
+
+	@Override
+	public int listSearchCount(SearchCriteria cri) throws Exception {
+		return dao.listSearchCount(cri);
+	}
+	
+	@Override
+	public List<String> getAttach(Integer bno) throws Exception {
+		return dao.getAttach(bno);
+	}
+
+}
